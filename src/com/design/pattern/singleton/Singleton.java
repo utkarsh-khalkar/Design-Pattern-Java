ead62e461b7ee4db330d9818e01563290f98f639
utkarsh-khalkar <utkarsh.khalkar@gmail.com>
2024-09-12 09:29:46 +0530

    Singleton pattern Modification

--- src/com/design/pattern/singleton/Singleton.java
+++ src/com/design/pattern/singleton/Singleton.java
@@ -1,13 +1,21 @@
 package com.design.pattern.singleton;
 
-public class Singleton {
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class Singleton implements Serializable {
     // This is Lazy of creation of Object..
     private static Singleton instance=null;
     // This Approach is Not Thread Safe
 
     // Constructor from  preventing Object creation
     private Singleton()
-    {}
+    {
+        if (instance != null) {
+            throw new RuntimeException();
+        }
+    }
 
     // Method for Object Creation
     public static Singleton getInstance()
@@ -18,12 +26,42 @@ public class Singleton {
 
         return instance;
     }
+    public Object readResolve()
+    {
+        return instance;
+    }
 }
 class Test{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException, ClassNotFoundException {
         Singleton singleton=Singleton.getInstance();
         System.out.println(singleton.hashCode());
+//        Singleton singleton1=Singleton.getInstance();
+//        System.out.println(singleton1.hashCode());
+
+
+        // Using Reflection API we can Break the Singleton Pattern.
+//        Constructor<Singleton> constructor=Singleton.class.getDeclaredConstructor();
+//        constructor.setAccessible(true);
+//        Singleton singleton1=constructor.newInstance();
+//
+//        System.out.println(singleton1.hashCode());
+
+
+        // Using Deserialization we can break the Singleton Pattern
+
+        // First We need to Serialize the Object then need to convert it into Deserialization
         Singleton singleton1=Singleton.getInstance();
-        System.out.println(singleton1.hashCode());
+
+        ObjectOutputStream objectOutputStream=new ObjectOutputStream(new
+                FileOutputStream("abc.txt"));
+
+        objectOutputStream.writeObject(singleton1);
+        System.out.println("Serialization  Done...");
+
+        ObjectInputStream objectInputStream=new ObjectInputStream(new FileInputStream("abc.txt"));
+
+        Singleton singleton2=(Singleton) objectInputStream.readObject();
+
+        System.out.println(singleton2.hashCode());
     }
 }
